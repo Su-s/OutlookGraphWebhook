cc670bf5c9719b9c918195167a3d23d6467353df
Sruthi JS <sruthy.js@visualyze.ai>
2024-01-22 14:31:34 +0530

    resolved issue in starting application

--- OutlookGraphWebhook/Program.cs
+++ OutlookGraphWebhook/Program.cs
@@ -6,9 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(configuration.GetSection("AzureAd"))
+    .AddMicrosoftIdentityWebApi(configuration)
     .EnableTokenAcquisitionToCallDownstreamApi()
     .AddMicrosoftGraph(configuration.GetSection("GraphApi"));
+builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,7 +27,7 @@ if (env.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
